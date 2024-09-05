b82a21ac1c8d10c301a7b249385b3d4a1378af89
Marcus <maje0017@stud.kea.dk>
2024-09-05 11:21:49 +0200

    næsten god

--- category.js
+++ category.js
@@ -1,1 +1,43 @@
+const productContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate").content;
+const categoryList = document.querySelector("#categoryList");
+const params = new URLSearchParams(document.location.search);
+const category = params.get("category");
+let url = undefined;
+if (params.has("category")) {
+  url = `https://kea-alt-del.dk/t7/api/products?category=${category}`;
+} else {
+  url = "https://kea-alt-del.dk/t7/api/products";
+}
 
+fetch("https://kea-alt-del.dk/t7/api/categories")
+  .then((response) => response.json())
+  .then((categories) => {
+    categories.forEach((category) => {
+      categoryList.innerHTML += `<li><a href="kategori.html?category=${category.category}">${category.category}</a></li>`;
+    });
+  });
+
+function duplicateTemplate(template, container) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((product) => {
+        const templateClone = template.cloneNode(true);
+
+        templateClone.querySelector(".category").textContent = product.category;
+        templateClone.querySelector(".price").textContent = product.price;
+        templateClone.querySelector(".brand").textContent = product.brandname;
+        templateClone.querySelector("#seemore").setAttribute("href", `produkt.html?productid=${product.id}`);
+        templateClone.querySelector("img").setAttribute("src", `https://kea-alt-del.dk/t7/images/jpg/640/${product.id}.jpg`);
+        if (product.soldout) {
+          templateClone.querySelector("#soldoutLabel").classList.add("soldout");
+        }
+
+        container.appendChild(templateClone);
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+duplicateTemplate(productTemplate, productContainer);
--- productlist.js
+++ productlist.js
@@ -4,6 +4,15 @@ const url = `https://kea-alt-del.dk/t7/api/products?limit=15`;
 const skabelon = document.querySelector("template").content;
 const container = document.querySelector("main");
 
+const params = new URLSearchParams(document.location.search);
+const category = params.get("category");
+
+if (params.has("category")) {
+  url = `https://kea-alt-del.dk/t7/api/products?category=${category}`;
+} else {
+  url = "https://kea-alt-del.dk/t7/api/products";
+}
+
 function hentData() {
   fetch(url)
     .then((res) => res.json())
@@ -17,7 +26,7 @@ function visProdukter(produkter) {
     kopi.querySelector("img").alt = produkt.productdisplayname;
     kopi.querySelector("h3").textContent = produkt.productdisplayname;
     kopi.querySelector(".price span").textContent = produkt.price;
-    kopi.querySelector("a").href = `product.html?id=${produkt.id}`;
+    kopi.querySelector("a").href = `produkt.html?id=${produkt.id}`;
     if (produkt.soldout) {
       kopi.querySelector("article").classList.add("soldOut");
     }
@@ -30,34 +39,3 @@ function visProdukter(produkter) {
     container.appendChild(kopi);
   });
 }
-
-/*
-<article class="smallProduct">
-                <img src="Billede_1.webp" alt="Fremhævet Bukser - Sondico">
-                <div class="featured-info">
-                    <h1>Sondico Bukser</h1>
-                    <h3>Flotte bukser fra Sondico, designet til både komfort og stil.</h3>
-                    <p class="pris">
-                        <span>Tidligere:</span> DKK 1595,-
-                    </p>
-                    <p class="discount-price">Nu DKK 1400,- <span>-34%</span></p>
-                    <a href="produkt.html" class="button">Køb nu</a>
-                </div>
-            </article>
-
-{
-  "id": 1163,
-  "gender": "Men",
-  "category": "Apparel",
-  "subcategory": "Topwear",
-  "articletype": "Tshirts",
-  "season": "Summer",
-  "productionyear": 2011,
-  "usagetype": "Sports",
-  "productdisplayname": "Sahara Team India Fanwear Round Neck Jersey",
-  "price": 895,
-  "discount": null,
-  "brandname": "Nike",
-  "soldout": 0
-}
-*/
